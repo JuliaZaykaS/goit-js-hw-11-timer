8a491d43ea7c14cb7b16a1fe68280915abad4f85
JuliaZaykaS <sys55@mail.ru>
2021-06-05 12:54:47 +0300

    сделала ссылки на таймер

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const refs = {
+    daysEl: document.querySelector('[data-value="days"]'),
+    hoursEl: document.querySelector('[data-value="hours"]'),
+    minutesEl: document.querySelector('[data-value="mins"]'),
+    secondsEl: document.querySelector('[data-value="secs"]'),
+}
+
