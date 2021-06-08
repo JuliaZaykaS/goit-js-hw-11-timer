7e3731cafbe0fb209bf11a08e9b6f6978a07fa27
JuliaZaykaS <sys55@mail.ru>
2021-06-08 15:05:55 +0300

    сделала таймер, пытаюсь прикрутить к шаблону

--- src/index.js
+++ src/index.js
@@ -57,51 +57,109 @@ const { daysEl, hoursEl, minutesEl, secondsEl, timerEl } = refs;
         constructor(selector, date) {
             this.selector = selector,
             this.targetDate = new Date(date),
-                this.start()
+            this.start()
 
         }
 
-    start() {
-        const currentDate = Date.now();
-        console.log(currentDate);
+        // start() {
+        //     setInterval(() => {
+
+        //         const result = this.getTimeComponents(this.getDate());
+        //         // console.log(result);
+        //         daysEl.textContent = result.days;
+        //         hoursEl.textContent = result.hours;
+        //         minutesEl.textContent = result.mins;
+        //         secondsEl.textContent = result.secs;
+
+        //             }, 1000);
+
+
+        // }
+        start() {
+
+            // const result = [this.getTimeComponents(this.getDate())];
+            // console.log(result);
+
+
+            // const timerMarkup = timerTpl(result);
+            // console.log(timerMarkup);
+            // document.body.insertAdjacentHTML('afterbegin', timerMarkup);
+            setInterval(() => {
+            const result = [this.getTimeComponents(this.getDate())];
+            console.log(result);
+
+
+            const timerMarkup = timerTpl(result);
+            console.log(timerMarkup);
+            // document.body.insertAdjacentHTML('afterbegin', timerMarkup);
+                // document.body.insertAdjacentElement('afterbegin', timerMarkup);
+
+                // console.log(result);
+                // const timerMarkup = timerTpl(result.days, result.hours, result.mins, result.secs);
+                // document.body.insertAdjacentHTML('afterbegin', timerMarkup);
+                // daysEl.textContent = result.days;
+                // hoursEl.textContent = result.hours;
+                // minutesEl.textContent = result.mins;
+                // secondsEl.textContent = result.secs;
+                // return result;
+
+            }, 1000);
+
+
+        }
+
+        getDate() {
+            const currentDate = Date.now();
+        // console.log(currentDate);
 
         // const targetDate = new Date('Jun 17, 2021').getTime();
         const targetDate = new Date(this.targetDate).getTime();
-        console.log(targetDate);
+        // console.log(targetDate);
 
         let time = targetDate - currentDate;
-        console.log(time);
+        // console.log(time);
         return time;
-    }
+        }
 
         getTimeComponents(time) {
 
         const days = Math.floor(time / (1000 * 60 * 60 * 24));
-        console.log(days);
+        // console.log(days);
         // daysEl.textContent = days;
 
         const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        console.log(hours);
+        // console.log(hours);
         // hoursEl.textContent = hours;
 
         const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        console.log(mins);
+        // console.log(mins);
         // minutesEl.textContent = mins;
 
         const secs = Math.floor((time % (1000 * 60)) / 1000);
-        console.log(secs);
+        // console.log(secs);
         // secondsEl.textContent = secs;
         return { days, hours, mins, secs };
+        // return timeToEnd;
+        // return [days, hours, mins, secs];
     }
-        getMarkUp({days, hours, mins, secs}) {
-            // this.getTimeComponents({days, hours, mins, secs});
+        // getMarkUp({ days, hours, mins, secs }) {
+        //     const result = this.getTimeComponents(this.getDate());
+        //     console.log(result);
+        //     // const timeToEnd = { days, hours, mins, secs };
+        // // getMarkUp(timeToEnd) {
+        //     // this.getTimeComponents({days, hours, mins, secs});
 
-            const timerMarkup = timerTpl(this.selector, days, hours, mins, secs );
+        //     // const timerMarkup = timerTpl(days, hours, mins, secs);
+        //     daysEl.textContent = result.days;
+        //     hoursEl.textContent = result.hours;
+        //     minutesEl.textContent = result.mins;
+        //     secondsEl.textContent = result.secs;
+
             // console.log(timerMarkup);
             // return document.body.insertAdjacentHTML('afterbegin', timerMarkup);
-            return document.body.insertAdjacentHTML('afterbegin', timerMarkup);
+            // return document.body.insertAdjacentHTML('afterbegin', timerMarkup);
             // return timerMarkup;
-        }
+        // }
         // setInterval(() => {
             // // start() {
 
@@ -114,9 +172,12 @@ const { daysEl, hoursEl, minutesEl, secondsEl, timerEl } = refs;
 
             // }
 
-    const timer = new CountdownTimer('#timer-1', 'Jun 17 2021');
-console.log(timer);
-    document.body.insertAdjacentHTML('afterbegin', timer.getMarkUp(timer.targetDate));
+const timer = new CountdownTimer('#timer-1', 'Jun 17 2021');
+// const timer = new CountdownTimer('#timer-1', 'Jan 30 2022');
+// console.log(timer);
+// console.log(timer.targetDate);
+// timer.getMarkUp(timer.targetDate);
+// document.body.insertAdjacentHTML('afterbegin', timer.getMarkUp(timer.targetDate));
 
 
 
